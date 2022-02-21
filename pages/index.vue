4745db4b3fa0cef86b71202b04b7a23b8575ad44
Flávio Garcia <flavioreisgf2010@hotmail.com>
2022-02-21 15:35:09 -0300

    Index page created. API consumed putting the data into a list.

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+        <li v-for="pokemon in pokemons.results" :key="pokemon.name">{{pokemon.name}} {{pokemon.url}}</li>
+    </ul>
+</div>
+</template>
+
+<script setup>
+
+const titulo = "Lista de Pokemons"
+
+const listPokemon = await fetch('https://pokeapi.co/api/v2/pokemon/')
+const pokemons = await listPokemon.json()
+
+
+</script>
+
+<style>
+
+</style>
