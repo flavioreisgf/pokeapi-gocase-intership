d7e06820d4781c0f5d7f3a3f616d9f009b977461
Flávio Garcia <flavioreisgf2010@hotmail.com>
2022-02-23 15:43:13 -0300

    Added a header and footer. Also included a link to the github repository

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-<div class="caixa">
-        <h1 class="centralizado"><img class="logo" src="https://i2.wp.com/multarte.com.br/wp-content/uploads/2019/03/pokemon-png-logo.png?fit=2000%2C736&ssl=1" alt="Logo Pokemon sem fundo"></h1>
-        <ul>
-            <li class="listaPoke" v-for="pokemon in listSearch" :key="pokemon.name"><a :href="'pokemon/' + pokemon.name"><img src="https://www.pinpng.com/pngs/m/8-82850_poke-ball-png-pokeball-png-transparent-png.png" width="20px" height="20px" alt="pokebola"> {{pokemon.name}}</a></li>
-        </ul>
-</div>
+    <header><h1 class="centralizado"><img class="logo" src="https://i2.wp.com/multarte.com.br/wp-content/uploads/2019/03/pokemon-png-logo.png?fit=2000%2C736&ssl=1" alt="Logo Pokemon sem fundo"></h1></header>
+    <main>
+        <div class="caixa">
+            
+            <ul>
+                <li class="listaPoke" v-for="pokemon in listSearch" :key="pokemon.name"><a :href="'pokemon/' + pokemon.name"><img src="https://www.pinpng.com/pngs/m/8-82850_poke-ball-png-pokeball-png-transparent-png.png" width="20px" height="20px" alt="pokebola"> {{pokemon.name}}</a></li>
+            </ul>
+    </div>
+    </main>
+
+    <footer> <a href="https://github.com/flavioreisgf/pokeapi-gocase-intership"> Github Project </a> </footer>
 </template>
 
 <script setup>
@@ -69,6 +74,17 @@ a:hover {
 }   
 .listaPoke:hover {
     transform:scale(1.2);
+}
+
+footer, header{
+     background: rgba(248, 227, 227, 0.452);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-transform: uppercase;
 }
 
 @media screen and (max-width: 540px) {
